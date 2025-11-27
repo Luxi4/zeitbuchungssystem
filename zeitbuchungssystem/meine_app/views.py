d9eb2fd3b3c139173a3e944edaec9c60c19246cc
Luxi4 <Cai_Lucy@teams.hs-ludwigsburg.de>
2025-11-27 14:33:51 +0100

    registrierung rüberkopiert

--- zeitbuchungssystem/meine_app/views.py
+++ zeitbuchungssystem/meine_app/views.py
@@ -1,18 +1,18 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from django.http import HttpResponse
-from datetime import datetime
-from django.http import HttpRequest
 
-def servus (request):
-	return HttpResponse("servus, schön dass es dich gibt!")
-
-def jetzt (request):
-	eben = datetime.now()
-	return HttpResponse(f"aktuelle Uhrzeit: {eben:%Hh %Mm %Ss}")
-
-def setcookie(request):
-	response = HttpResponse("hier wurde ein Keks gesetzt")
-	response["Content-Type"] = "text/plain"
-	response.set_cookie("keks", ":-)")
-	response.set_cookie("vergammelterkeks", ":-(")
-	return response
+def register(request, template_name='meine_app/register.html', next_page_name=None):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            if next_page_name is None:
+                next_page = '/'
+            else:
+                next_page = reverse(next_page_name)
+            return HttpResponseRedirect(next_page)
+    else:
+        form = UserCreationForm()
+    return render(request, template_name, {'form': form})
